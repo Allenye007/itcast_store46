ee2f6e5b0aed1721d37265a0a34658e570307158
goddlts <54337467@qq.com>
2018-07-08 16:17:28 +0800

    搜索区域

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -6,6 +6,16 @@
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 搜索区域 -->
+    <el-row class="searchArea">
+      <el-col :span="24">
+        <el-input class="searchInput" clearable placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -18,5 +28,12 @@ export default {
 <style>
 .box-card {
   height: 100%;
+}
+.searchArea {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.searchArea .searchInput {
+  width: 350px;
 }
 </style>
