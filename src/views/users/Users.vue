67d3e4b5ac6a3422067b6ac11b7db78fb6ed50f2
goddlts <54337467@qq.com>
2018-07-08 16:11:00 +0800

    面包屑和卡片

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    用户列表
-  </div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </el-card>
 </template>
 
 <script>
@@ -11,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+.box-card {
+  height: 100%;
+}
 </style>
