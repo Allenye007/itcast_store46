4e753171126efbbdbfba98bdd85fd47b188053da
goddlts <54337467@qq.com>
2018-07-11 17:04:37 +0800

    商品分类-显示列表数据

--- src/views/goods/Category.vue
+++ src/views/goods/Category.vue
@@ -11,21 +11,29 @@
 
     <!-- 表格 -->
     <el-table
+      stripe
+      border
       :data="list"
       style="width: 100%">
       <el-table-column
-        prop="date"
+        prop="cat_name"
         label="分类名称"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="name"
         label="级别"
         width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.cat_level === 0">一级</span>
+          <span v-else-if="scope.row.cat_level === 1">二级</span>
+          <span v-else-if="scope.row.cat_level === 2">三级</span>
+        </template>
       </el-table-column>
       <el-table-column
-        prop="address"
         label="是否有效">
+        <template slot-scope="scope">
+          {{ scope.row.cat_deleted ? '无效' : '有效' }}
+        </template>
       </el-table-column>
       <el-table-column
         label="操作">
@@ -44,6 +52,17 @@ export default {
     return {
       list: []
     };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      const { data: resData } = await this.$http.get('categories?type=3');
+
+      const { data } = resData;
+      this.list = data;
+    }
   }
 };
 </script>
