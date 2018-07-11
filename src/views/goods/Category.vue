b313fa96d0fb5f9b9fd1ea74f77ed2bb9644405e
goddlts <54337467@qq.com>
2018-07-11 16:51:59 +0800

    商品分类-搭建界面

--- src/views/goods/Category.vue
+++ src/views/goods/Category.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-    商品分类
-  </div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="商品管理" level2="商品分类"></my-breadcrumb>
+
+    <el-row class="row-add">
+      <el-col :span="24">
+        <el-button type="success" plain>添加分类</el-button>
+      </el-col>
+    </el-row>
+
+    <!-- 表格 -->
+    <el-table
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="分类名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="级别"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="是否有效">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button plain size="mini" type="primary" icon="el-icon-edit" ></el-button>
+          <el-button plain size="mini" type="danger" icon="el-icon-delete" ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: []
+    };
+  }
 };
 </script>
 
 <style>
-
+.row-add {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
