e92991f79df817689a3df3d3332484f1a4b40f59
lipengzhou <lpzmail@163.com>
2018-07-13 15:21:26 +0800

    添加商品-配置步骤条组件和标签页组件

--- src/views/goods/Add.vue
+++ src/views/goods/Add.vue
@@ -5,6 +5,22 @@
 
     <el-row class="row-add">
     </el-row>
+
+    <!-- 步骤条 -->
+    <el-steps :active="1" finish-status="success" align-center>
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+    </el-steps>
+    <!-- /步骤条 -->
+
+    <!-- 标签页 -->
+    <el-tabs tab-position="left" style="height: 200px;">
+      <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+    </el-tabs>
+    <!-- /标签页 -->
   </el-card>
 </template>
 
